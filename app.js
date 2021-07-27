28f78e481b8c4f1ba9a28861787f6a5a821fc298
gedunlap <g.e.dunlap90@gmail.com>
2021-07-27 09:41:56 -0400

    log it and arr

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = new Vue({
     data: {
         hello: "Hello Garrett",
         show: false,
+        arr: [1,2,3,4,5,6],
+        input: ""
     },
     methods: {
         hideAndSeek: function(){
             this.show = !this.show
+        },
+        logIt: function(){
+            console.log(this.input)
         }
     }
 })
