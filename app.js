e60f171a6a60ff74a80cc64e9f49c9a3e988d796
gedunlap <g.e.dunlap90@gmail.com>
2021-07-27 11:28:07 -0400

    using components

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
+Vue.component('hellogarrett', {
+    template: "<h1>Hello Garrett</h1>"
+})
+
 const app = new Vue({
     el: "#app",
-    data: {
-        hello: "Hello Garrett",
-        show: false,
-        arr: [1,2,3,4,5,6],
-        input: ""
-    },
-    methods: {
-        hideAndSeek: function(){
-            this.show = !this.show
-        },
-        logIt: function(){
-            console.log(this.input)
-        }
-    }
 })
