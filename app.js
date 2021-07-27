14244aa839f736cda187112dab1e901fdbbeed64
gedunlap <g.e.dunlap90@gmail.com>
2021-07-27 09:35:52 -0400

    hide and seek

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const app = new Vue({
     el: "#app",
     data: {
-        hello: "Hello Garrett"
+        hello: "Hello Garrett",
+        show: false,
+    },
+    methods: {
+        hideAndSeek: function(){
+            this.show = !this.show
+        }
     }
 })
