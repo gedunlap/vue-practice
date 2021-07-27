adac5e1fefb0fec7863d1ef2a17d57686331e3c6
gedunlap <g.e.dunlap90@gmail.com>
2021-07-27 11:39:34 -0400

    data and computed

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 Vue.component('hellogarrett', {
-    template: "<h1>{{cheese}}</h1>",
-    props: ['cheese']
+    template: `<div>
+    <h1>{{hello}} {{cheese}} {{age}} {{doubleAge}}</h1> 
+    <input text="number" v-model="age"/></div>`,
+    props: ['cheese'],
+    data: function(){
+        return {
+            hello: "Hello",
+            age: 31
+        }
+    },
+    methods: {},
+    computed: {
+        doubleAge: function(){
+            return this.age * 2
+        }
+    }
 })
 
 const app = new Vue({
